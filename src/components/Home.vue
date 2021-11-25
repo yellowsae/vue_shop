2ab97dd44dad0d87747c9502b8a7899dd72ef23d
yellowsae <yunhai0644@163.com>
2021-11-25 13:25:59 +0800

    login

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,33 @@
 <template>
  <div>
-   这是 Home组件
+<!--   实现退出功能 -->
+   <el-button type="info"  @click='logout'>退出</el-button>
  </div>
 </template>
 
 <script>
 export default {
-  name: "Home"
+  name: "Home",
+  methods: {
+    logout() {
+      // 清空token
+      window.sessionStorage.clear();
+      // 强制返回登录页面
+      this.$router.push('/login')
+      this.$message({
+        showClose: true,
+        message: '你已退出登录',
+      });
+    }
+  }
 }
+
+/**
+ * 1. 登录退出功能 （2步骤）
+ * 清空token : window.sessionStorage.clear();
+ * 跳转到登录页面 : this.$router.push('/login')
+ *
+ */
 </script>
 
 <style scoped>
